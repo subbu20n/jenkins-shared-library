cf38842800f8f892608370274769c87543b0cb4f
subbu <subbunandyala48@gmail.com>
2025-08-27 13:15:50 +0530

    jenkins

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -29,7 +29,7 @@ def call(Map configMap) {
                 }
             }    
         }
-        stage('Test'){
+        stage('Test'){ 
             steps {
                 script {
                       echo "Testing"
